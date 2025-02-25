2a5291c53b027bb8212150dbec126ba7c1d99034
Sam Morrisroe <sammorrisroe@gmail.com>
2025-02-25 11:51:01 -0800

    created vent and lamp

---
+++ Assets/Scripts/LampController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class LampController : MonoBehaviour
+{
+    public float speed = 5;
+    public bool lit;
+    public Transform wickPosition;
+
+    private InputAction moveAction;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (lit) {
+            Move();
+        }
+    }
+
+    private void Move() {
+        Vector2 moveInput = moveAction.ReadValue<Vector2>();
+
+        Vector3 velocity = rb.linearVelocity;
+        velocity.x = moveInput.x * speed;
+
+        rb.linearVelocity = velocity;
+    }
+}
