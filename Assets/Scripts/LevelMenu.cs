4d3258a9e9da5bea32aac397a0544cb9ae71892e
Casey Avila <casey@theavilas.org>
2025-03-20 17:21:39 -0700

    First build

--- Assets/Scripts/LevelMenu.cs
+++ Assets/Scripts/LevelMenu.cs
@@ -16,12 +16,7 @@ public class LevelMenu : MonoBehaviour
 
         for (int i = 0; i < buttons.Length; i++)
         {
-            buttons[i].interactable = false;
-        }
-
-        for (int i = 0; i < unlockedLevel; i++)
-        {
-            buttons[i].interactable = true;
+            buttons[i].interactable = i < unlockedLevel;
         }
     }
 
